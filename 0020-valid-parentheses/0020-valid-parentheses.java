d1a2ab696bc03b7ba2b9156b54eeb53ef2fdef7d
Uday Kumar Reddy <udaykumarreddy939@gmail.com>
2025-06-03 15:18:03 +0530

    Time: 2 ms (97.27%), Space: 41.7 MB (66.01%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char x = st.peek();
+                if((ch == ')' && x =='(') || (ch == '}' && x =='{') || (ch == ']' && x =='[')){
+                    st.pop();
+                }else {
+                    return false;
+                }
+            }
+        }
+        if(st.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
