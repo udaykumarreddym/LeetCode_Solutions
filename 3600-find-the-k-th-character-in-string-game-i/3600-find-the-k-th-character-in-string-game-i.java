b8aade6d2b5f7b5eb0c0a71c8180963d9c0cff42
Uday Kumar Reddy <udaykumarreddy939@gmail.com>
2025-07-03 21:57:36 +0530

    Time: 144 ms (5.14%), Space: 43.1 MB (43.8%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.java
@@ -1,15 +1,15 @@
 class Solution {
     public char kthCharacter(int k) {
         String word = "a";
-        while(word.length() < k){
-            String newString = "";
-            for(int i=0;i<word.length();i++){
+        while (word.length() < k) {
+            StringBuilder newString = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
                 char ch = word.charAt(i);
-                char next = (char)((ch-'a' + 1) % 26 + 'a');
-                newString += next;
+                char next = (char) (((ch - 'a' + 1) % 26) + 'a'); // Fix here
+                newString.append(next);
             }
-            word += newString;
+            word += newString.toString();
         }
-        return word.charAt(k-1);
+        return word.charAt(k - 1);
     }
 }
