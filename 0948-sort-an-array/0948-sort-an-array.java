4e07e8adf178664396e0a7088a30d30243dc1f44
Uday Kumar Reddy <udaykumarreddy939@gmail.com>
2025-07-04 22:23:13 +0530

    Time: 18 ms (91.5%), Space: 55.5 MB (86.26%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,20 +1,6 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int n = nums.length;
-        for(int i=0;i<n-1;i++){
-            boolean bool = false;
-            for(int j=0;j<n-i-1;j++){
-                if(nums[j] > nums[j+1]){
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                    bool = true;
-                }
-            }
-            if(!bool){
-                return nums;
-            }
-        }
+        Arrays.sort(nums);
         return nums;
     }
 }
