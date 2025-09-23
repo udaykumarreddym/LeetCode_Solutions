2e9e8811777ea4862deaa0dfc4e1632615470590
Uday Kumar Reddy <udaykumarreddy939@gmail.com>
2025-09-23 23:10:22 +0530

    Time: 0 ms (100%), Space: 43.4 MB (19.1%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int ans = n,start = 0,end = n-1;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(nums[mid] >= target){
+                ans = mid;
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
