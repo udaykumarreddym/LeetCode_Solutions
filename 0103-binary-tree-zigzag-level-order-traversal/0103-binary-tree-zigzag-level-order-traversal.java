396834844965d280f465304171cf43ced449eaff
Uday Kumar Reddy <udaykumarreddy939@gmail.com>
2025-12-31 07:01:37 +0530

    Time: 0 ms (100%), Space: 43.7 MB (58.73%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,24 +15,24 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if(root == null) return ans;
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
-        boolean ltr = true;
+        boolean ltor = true;
         while(!q.isEmpty()){
-            int len = q.size();
-            List<Integer> level = new ArrayList<>();
-            for(int i=0;i<len;i++){
+            int size = q.size();
+            Integer[] level = new Integer[size];
+            for(int i=0;i<size;i++){
                 TreeNode node = q.poll();
-                level.add(node.val);
+                int idx = ltor ? i : size-1-i;
+                level[idx] = node.val;
                 if(node.left != null) q.offer(node.left);
                 if(node.right != null) q.offer(node.right);
             }
-            if(!ltr) Collections.reverse(level);
-            ltr = !ltr;
-            ans.add(level);
+            ltor = !ltor;
+            res.add(Arrays.asList(level));
         }
-        return ans;
+        return res;
     }
 }
